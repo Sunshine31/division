89cebe290a5f6d26e48a9c5a96409ca21b803c2e
Sasha Levshin <ocarin31@yandex.ru>
2022-12-14 00:40:48 +0700

    Registration and login forms

---
+++ lib/division/accounts/user.ex
@@ -0,0 +1,44 @@
+defmodule Division.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :password_hash, :string
+    field :username, :string
+
+    # Virtual fields 
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :password_hash])
+    |> validate_required([:username, :password_hash])
+    |> unique_constraint(:username)
+    |> validate_length(:password, min: 6)
+    |> validate_confirmation(:password)
+    |> validate_format(:username, ~r/^[a-z0-9][a-z0-9]+[a-z0-9]$/i)
+    |> validate_length(:username, min: 6)
+    |> downcase_username
+    |> hash_password
+  end
+
+  defp downcase_username(changeset) do
+    update_change(changeset, :username, &String.downcase/1)
+  end
+  
+  defp hash_password(changeset) do
+    password = get_change(changeset, :password)
+    if password do
+      password_hash = Encryption.hash_password(password)
+      put_change(changeset, :password_hash, password_hash)
+    else
+      changeset
+    end
+  end
+
+end
